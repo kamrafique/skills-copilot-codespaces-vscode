4189ce995326608f869d5084687d78c2b0392ee2
kamrafique <24291469+kamrafique@users.noreply.github.com>
2023-11-01 21:32:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 30,
+        skills: ["JS", "CSS", "HTML", "JAVA"],
+        address: {
+            city: "Paris",
+            country: "France",
+            street: "Rue de la Paix"
+        }
+    };
+    return member;
+}    
